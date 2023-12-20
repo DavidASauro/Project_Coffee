e3a41a3c122ff94b075ebd75ac3166059524aebd
Nick <67390674+DDoSdbyDoor@users.noreply.github.com>
2023-12-20 14:24:11 -0500

    Start of controller
    +character
    +first script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello nerd");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+ 
+
+
+
+    }
+}
